e2bc0507d5f5a6f811d538cd8f005a33b0727203
czheisenberg <2065924157@qq.com>
2024-05-25 20:33:56 +0800

    vue多环境配置，增加dev and prod配置文件，编译和启动命令增加多环境

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,4 +5,8 @@ import 'ant-design-vue/dist/reset.css';
 import router from './router'
 import store from './store'
 
-createApp(App).use(Antd).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(Antd).use(store).use(router).mount('#app')
+
+console.log('环境: ', process.env.NODE_ENV);
+console.log('服务端地址: ', process.env.VUE_APP_SERVER );
