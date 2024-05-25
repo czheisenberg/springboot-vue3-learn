85db3b07bd75956a8b6e5bdac0bf77d075e2b39d
czheisenberg <2065924157@qq.com>
2024-05-25 17:09:04 +0800

    Ant Design vue list 组件添加

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -50,22 +50,82 @@
   <a-layout-content
       :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
   >
-    <pre>
-      {{ebooks}}
-    </pre>
+
+    <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+      <template #footer>
+        <div>
+          <b>ant design vue</b>
+          footer part
+        </div>
+      </template>
+      <template #renderItem="{ item }">
+        <a-list-item key="item.title">
+          <template #actions>
+          <span v-for="{ icon, text } in actions" :key="icon">
+            <component :is="icon" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+          </template>
+          <template #extra>
+            <img
+                width="272"
+                alt="logo"
+                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+            />
+          </template>
+          <a-list-item-meta :description="item.description">
+            <template #title>
+              <a :href="item.href">{{ item.title }}</a>
+            </template>
+            <template #avatar><a-avatar :src="item.avatar" /></template>
+          </a-list-item-meta>
+          {{ item.content }}
+        </a-list-item>
+      </template>
+    </a-list>
+
   </a-layout-content>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent , onMounted, ref} from 'vue';
+import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
 import axios from 'axios';
+
+// 前端数据
+const listData: Record<string, string>[] = [];
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com/',
+    title: `ant design vue part ${i}`,
+    avatar: 'https://joeschmoe.io/api/v1/random',
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+  });
+}
+// pagination 分页
+const pagination = {
+  onChange: (page: number) => {
+    console.log(page);
+  },
+  pageSize: 3,
+};
+// 图标
+const actions: Record<string, any>[] = [
+  { icon: StarOutlined, text: '156' },
+  { icon: LikeOutlined, text: '156' },
+  { icon: MessageOutlined, text: '666' },
+];
 
 export default defineComponent({
   name: 'HomeView',
   setup(){
     console.log("setup");
     const ebooks = ref()
+
 
     // onMounted()是一个生命周期函数
     onMounted(()=>{
@@ -79,7 +139,15 @@ export default defineComponent({
       });
     });
 
-    return{ebooks}
+    return{
+      ebooks,
+
+    //   返回前端页面数据
+      listData,
+      pagination,
+      actions
+
+    }
   }
 });
 </script>
