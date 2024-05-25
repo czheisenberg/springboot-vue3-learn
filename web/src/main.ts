ded8ed1b2434c5be97d78bc157661e5cd67c83b5
czheisenberg <2065924157@qq.com>
2024-05-25 14:36:21 +0800

    网站布局

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+import Antd from 'ant-design-vue';
 import App from './App.vue'
+import 'ant-design-vue/dist/reset.css';
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(Antd).use(store).use(router).mount('#app')
