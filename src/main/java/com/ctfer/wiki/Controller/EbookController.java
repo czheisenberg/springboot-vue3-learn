1a07f0772ee13b7edfc383ea15ea0e58e1bf80e7
czheisenberg <2065924157@qq.com>
2024-05-25 12:15:38 +0800

    ebook/list?name=xxx模糊查询

--- src/main/java/com/ctfer/wiki/Controller/EbookController.java
+++ src/main/java/com/ctfer/wiki/Controller/EbookController.java
@@ -5,20 +5,22 @@ import com.ctfer.wiki.resp.CommonResp;
 import com.ctfer.wiki.service.EbookService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/ebook")
 public class EbookController {
 
     @Resource
     private EbookService ebookService;
 
-   @GetMapping("/ebook/list")
-    public CommonResp list(){
+   @GetMapping("/list")
+    public CommonResp list(String name){
        CommonResp<List<Ebook>> resp = new CommonResp<>();
-       List<Ebook> list = ebookService.list();
+       List<Ebook> list = ebookService.list(name);
        resp.setContent(list);
 
        return resp;
--- src/main/java/com/ctfer/wiki/service/EbookService.java
+++ src/main/java/com/ctfer/wiki/service/EbookService.java
@@ -1,6 +1,7 @@
 package com.ctfer.wiki.service;
 
 import com.ctfer.wiki.domain.Ebook;
+import com.ctfer.wiki.domain.EbookExample;
 import com.ctfer.wiki.mapper.EbookMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,14 @@ public class EbookService {
     @Autowired
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
+    public List<Ebook> list(String name){
+        EbookExample ebookExample = new EbookExample();
+        // Criteria 相当于 where条件
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
 
-        return ebookMapper.selectByExample(null);
+        // adnNameLike like模糊匹配
+        criteria.andNameLike("%"+name+"%");
+
+        return ebookMapper.selectByExample(ebookExample);
     }
 }
