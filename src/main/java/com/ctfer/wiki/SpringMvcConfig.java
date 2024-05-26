7dece5d36a189affa391eab7cc100954fd06ffee
czheisenberg <2065924157@qq.com>
2024-05-26 10:06:19 +0800

    增加拦截器，打印接口耗时

---
+++ src/main/java/com/ctfer/wiki/SpringMvcConfig.java
@@ -0,0 +1,20 @@
+package com.ctfer.wiki;
+
+import com.ctfer.wiki.interceptor.LogInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Resource
+    LogInterceptor logInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+//        .addPathPatterns("/**")所有请求，excludePathPatterns("/login")排除/login
+        registry.addInterceptor(logInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/login");
+    }
+}
