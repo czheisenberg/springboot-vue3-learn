9a17a25cbd19707f6c6dbc1ca09e85f1129a4482
czheisenberg <2065924157@qq.com>
2024-05-30 16:25:01 +0800

    day6: wangeditor编辑器,加载问题待修复

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -101,6 +101,9 @@
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort" />
       </a-form-item>
+      <a-form-item label="内容">
+        <div id="content"></div>
+      </a-form-item>
     </a-form>
   </a-modal>
 </template>
@@ -111,6 +114,7 @@ import axios from 'axios';
 import { message } from 'ant-design-vue';
 import {Tool} from "@/util/tool";
 import {useRoute} from "vue-router";
+import E from 'wangeditor';
 
 export default defineComponent({
   name: 'AdminDoc',
@@ -186,6 +190,12 @@ export default defineComponent({
     const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
+    let editor: E;
+    // const editor = new E('#content');
+    // editor = new E('#content');
+
+
+
     const handleModalOk = () => {
       modalLoading.value = true;
       axios.post("/doc/save", doc.value).then((response) => {
@@ -280,6 +290,11 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+
+      // setTimeout(function (){
+      //     editor = new E('#content');
+      //     editor.create();
+      // }, 100);
     };
 
     /**
@@ -295,6 +310,11 @@ export default defineComponent({
 
       // 为选择树添加一个"无"
       treeSelectData.value.unshift({id: 0, name: '无'});
+
+      // setTimeout(function (){
+      //   editor = new E('#content');
+      //   // editor.create();
+      // }, 100);
     };
 
     const handleDelete = (id: number) => {
@@ -312,6 +332,7 @@ export default defineComponent({
     };
 
     onMounted(() => {
+      // editor.create();
       handleQuery();
     });
 
