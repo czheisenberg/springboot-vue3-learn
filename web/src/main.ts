45faf848b9418b995aed0d6104745736c2764a4a
czheisenberg <2065924157@qq.com>
2024-05-31 16:51:19 +0800

    day7: 添加图片全局载入

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import Antd from 'ant-design-vue';
 import App from './App.vue';
 import 'ant-design-vue/dist/reset.css';
+import * as Icons from '@ant-design/icons-vue';
 import router from './router';
 import store from './store';
 import axios from 'axios';
@@ -28,6 +29,12 @@ axios.interceptors.response.use(function(response){
 
 const app = createApp(App);
 app.use(Antd).use(store).use(router).mount('#app')
+
+// 全局使用图标
+const icons: any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
 
 console.log('环境: ', process.env.NODE_ENV);
 console.log('服务端地址: ', process.env.VUE_APP_SERVER );
