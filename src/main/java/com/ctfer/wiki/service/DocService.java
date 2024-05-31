a9e45b7e7365b18d15a0c67a4d90609dd4c3532e
czheisenberg <2065924157@qq.com>
2024-05-31 10:31:23 +0800

    day7: 增加文档保存功能

--- src/main/java/com/ctfer/wiki/service/DocService.java
+++ src/main/java/com/ctfer/wiki/service/DocService.java
@@ -1,7 +1,9 @@
 package com.ctfer.wiki.service;
 
+import com.ctfer.wiki.domain.Content;
 import com.ctfer.wiki.domain.Doc;
 import com.ctfer.wiki.domain.DocExample;
+import com.ctfer.wiki.mapper.ContentMapper;
 import com.ctfer.wiki.mapper.DocMapper;
 import com.ctfer.wiki.req.DocQueryReq;
 import com.ctfer.wiki.req.DocSaveReq;
@@ -25,6 +27,9 @@ public class DocService {
 
     @Resource
     private DocMapper docMapper;
+
+    @Resource
+    private ContentMapper contentMapper;
 
     @Resource
     private SnowFlake snowFlake;
@@ -74,13 +79,21 @@ public class DocService {
      */
     public void save(DocSaveReq req) {
         Doc doc = CopyUtil.copy(req, Doc.class);
+        Content content = CopyUtil.copy(req, Content.class);
         if (ObjectUtils.isEmpty(req.getId())) {
             // 新增
             doc.setId(snowFlake.nextId());
             docMapper.insert(doc);
+            // 新增内容, doc.id === content.id doc表的id和content表的id是一样的,所以 content.id等于doc.getId()
+            content.setId(doc.getId());
+            contentMapper.insert(content);
         } else {
             // 更新
             docMapper.updateByPrimaryKey(doc);
+            int count = contentMapper.updateByPrimaryKeyWithBLOBs(content);
+            if (count == 0){
+                contentMapper.insert(content);
+            }
         }
     }
 
--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -206,7 +206,8 @@ export default defineComponent({
     // 因为树选择组件的属性状态，会随当前编辑的节点变化，所以单独声明一个响应式变量
     const treeSelectData = ref();
     treeSelectData.value = [];
-    const doc = ref({});
+    const doc = ref();
+    doc.value = {};
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     // let editor: E;
@@ -218,6 +219,8 @@ export default defineComponent({
 
     const handleSave = () => {
       modalLoading.value = true;
+      doc.value.content = editor.txt.html();  // editor.txt.html() 获取输入的内容,官方文档有
+      console.log("content: ", doc.value.content);
       axios.post("/doc/save", doc.value).then((response) => {
         modalLoading.value = false;
         const data = response.data; // data = commonResp
