63dd8bcc7424e8757ce3f3cb065934d7773fd204
czheisenberg <2065924157@qq.com>
2024-05-31 15:05:58 +0800

    day7: 使用树形组件选择文档树,遗留bug,文档树应该只查当前选中的电子书内容,而不是全部

--- web/src/views/Doc.vue
+++ web/src/views/Doc.vue
@@ -1,16 +1,82 @@
 <template>
   <a-layout>
     <a-layout-content :style="{background: 'fff', padding: '24px', margin: 0, minHeight: '280px'}">
-      <div class="doc">
-          <h1>欢迎来到文档页面</h1>
-      </div>
+      <a-row>
+        <a-col :span="6">
+          <a-tree-select
+              style="width: 30%"
+              v-if="level1.length > 0"
+              :tree-data="level1"
+              @select="onSelect"
+              :replaceFields="{label: 'name',value: 'id'}"
+              tree-default-expand-all
+              :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+              placeholder="Please select"
+          >
+          </a-tree-select>
+        </a-col>
+        <a-col :span="18">
+        </a-col>
+      </a-row>
     </a-layout-content>
   </a-layout>
 
 </template>
 
-<script>
+<script lang="ts">
+  import {defineComponent, onMounted, ref, createVNode} from "vue";
+  import axios from 'axios';
+  import {message} from 'ant-design-vue';
+  import {Tool} from '@/util/tool';
+  import {useRoute} from "vue-router";
 
+  export default defineComponent({
+    name: 'Doc',
+    setup(){
+      const route = useRoute();
+      const docs = ref();
+      /**
+       * 一级文档树，children属性就是二级文档
+       * [{
+       *   id: "",
+       *   name: "",
+       *   children: [{
+       *     id: "",
+       *     name: "",
+       *   }]
+       * }]
+       */
+      const level1 = ref(); // 一级文档树，children属性就是二级文档
+      level1.value = [];
+
+      /**
+       * 数据查询
+       **/
+      const handleQuery = () => {
+        axios.get("/doc/all").then((response) => {
+          const data = response.data;
+          if (data.success) {
+            docs.value = data.content;
+            // console.log("原始数组：", docs.value);
+
+            level1.value = [];
+            level1.value = Tool.array2Tree(docs.value, 0);
+            console.log("树形结构：", level1);
+          } else {
+            message.error(data.message);
+          }
+        });
+      };
+
+      onMounted(() => {
+        handleQuery();
+      });
+
+      return{
+        level1,
+      }
+    }
+  })
 </script>
 
 <style>
