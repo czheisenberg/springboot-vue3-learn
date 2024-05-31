e8bc08e97c8246b007534d2d3a8a7d6ce842d979
czheisenberg <2065924157@qq.com>
2024-05-31 17:17:35 +0800

    day7: doc修改了background:'#fff'

--- web/src/views/Doc.vue
+++ web/src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
   <a-layout>
-    <a-layout-content :style="{background: 'fff', padding: '24px', margin: 0, minHeight: '280px'}">
+    <a-layout-content :style="{background: '#fff', padding: '24px', margin: 0, minHeight: '280px'}">
       <a-row>
         <a-col :span="6">
 <!--          <a-tree-select-->
